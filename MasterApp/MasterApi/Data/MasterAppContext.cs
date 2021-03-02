8a04d4dd7f6798c47d957f011bbdf9eb7d8e61e4
sarathiidss <sarathi@idsoftsource.com>
2021-03-02 22:38:21 +0530

    Init App

---
+++ MasterApp/MasterApi/Data/MasterAppContext.cs
@@ -0,0 +1,20 @@
+﻿using MasterApi.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterApi.Data
+{
+    public class MasterAppContext : DbContext
+    {
+        public MasterAppContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<User> Users { get; set; }
+    }
+}
